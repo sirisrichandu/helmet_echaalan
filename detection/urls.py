aee03d2bc6e66f8b81e8ec78e706726ad7fb957d
Susmithakanukolanu20 <kanukolanususmitha20@gmail.com>
2026-01-19 08:48:00 +0530

    Define root URL for detection app upload view

--- detection/urls.py
+++ detection/urls.py
@@ -1,13 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.shortcuts import redirect
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    # ROOT → detection home
-    path('', lambda request: redirect('/detect/')),
-
-    path('admin/', admin.site.urls),
-    path('detect/', include('detection.urls')),
-    path('accounts/', include('accounts.urls')),
-    path('challan/', include('challan.urls')),
-]
+    path('', views.upload_image, name='upload_image'),
+]  commit message for this
