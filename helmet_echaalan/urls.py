97bebf7a316b3c7a68120abf23524f26fd8cb0e3
Susmithakanukolanu20 <kanukolanususmitha20@gmail.com>
2026-01-21 15:41:41 +0530

    New Update urls.py

--- helmet_echaalan/urls.py
+++ helmet_echaalan/urls.py
@@ -1,20 +1,3 @@
-"""
-URL configuration for helmet_echaalan project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -22,17 +5,15 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Helmet E-Challan System is Live")
-urlpatterns = [
-    # ❌ Do NOT expose detection / webcam
-    # ❌ Do NOT expose admin to public (optional)
 
-    # ✅ Public challan system
-    path('', include('challan.urls')),
-    path('', home),                 # ✅ FIX
+urlpatterns = [
+    # Home page
+    path('', home, name='home'),
+
+    # Public challan search
     path('challan/', include('challan.urls')),
 ]
 
 # Serve media only in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
