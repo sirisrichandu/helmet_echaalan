f14aed4e0fe2093f8090c08a837a78bc1fe821c1
unknown <sirisrichandu@gmail.com>
2026-01-21 13:00:12 +0530

    Public version is ready

--- helmet_echaalan/settings.py
+++ helmet_echaalan/settings.py
@@ -38,7 +38,6 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -56,7 +55,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'accounts',
+    'vehicles',
+    'detection',
     'challan',   # ✅ users can view challans
 ]
 
