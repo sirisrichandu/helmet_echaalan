f8e9e5bee8ad3b191a57449240a0455cc48dd910
unknown <sirisrichandu@gmail.com>
2026-01-21 20:40:55 +0530

    updated cloud in public version

--- challan/urls.py
+++ challan/urls.py
@@ -1,6 +1,7 @@
+
 from django.urls import path
 from .views import search_challan
 
 urlpatterns = [
-     path('search/', search_challan, name='search_challan'),
+    path('', search_challan, name='search_challan'),
 ]
--- challan/views.py
+++ challan/views.py
@@ -2,20 +2,22 @@ from django.shortcuts import render
 from .models import Challan
 
 def search_challan(request):
-    query = request.GET.get('q', '').strip()
-    search_type = request.GET.get('type', 'challan')
+    query = request.GET.get("q")
+    search_type = request.GET.get("type")
 
     challans = []
 
     if query:
-        if search_type == 'challan':
-            challans = Challan.objects.filter(echallan_number=query)
-        elif search_type == 'vehicle':
-            challans = Challan.objects.filter(
+        if search_type == "vehicle":
+            challans = Challan.objects.select_related("violation").filter(
                 violation__vehicle_number__iexact=query
             )
+        else:
+            challans = Challan.objects.select_related("violation").filter(
+                echallan_number__iexact=query
+            )
 
-    return render(request, 'challan/search.html', {
-        'challans': challans,
-        'query': query,
+    return render(request, "challan/search.html", {
+        "challans": challans,
+        "query": query,
     })
