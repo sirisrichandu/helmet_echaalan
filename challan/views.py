2b171ca580bb775da90519b02a3dc9490d2833c8
Susmithakanukolanu20 <kanukolanususmitha20@gmail.com>
2026-01-21 16:34:22 +0530

    SSS Update views.py

--- challan/views.py
+++ challan/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render
 from .models import Challan
 
-
 def search_challan(request):
     query = request.GET.get('q', '').strip()
+    search_type = request.GET.get('type', 'challan')
+
     challans = []
 
     if query:
-        challans = Challan.objects.filter(
-            echallan_number__iexact=query
-        )
+        if search_type == 'challan':
+            challans = Challan.objects.filter(echallan_number=query)
+        elif search_type == 'vehicle':
+            challans = Challan.objects.filter(
+                violation__vehicle_number__iexact=query
+            )
 
-    return render(
-        request,
-        'challan/search.html',
-        {
-            'challans': challans,
-            'query': query
-        }
-    )
+    return render(request, 'challan/search.html', {
+        'challans': challans,
+        'query': query,
+    })
