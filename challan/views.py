279c9314e677eb0550d5d293d66389abe30d1900
unknown <sirisrichandu@gmail.com>
2026-01-22 07:39:20 +0530

    updated views challan

--- challan/views.py
+++ challan/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render
-from django.db.models import Q
 from .models import Challan
 
 def search_challan(request):
-    query = request.GET.get("q")
-    search_type = request.GET.get("type")  # vehicle / challan
+    query = request.GET.get("q")   # GET, not POST
+    search_type = request.GET.get("type")
 
     challans = []
 
     if query:
         if search_type == "vehicle":
             challans = Challan.objects.select_related("violation").filter(
-                violation__vehicle_number__iexact=query
+                violation__vehicle_number__iexact=query.strip()
             )
-        else:  # challan number
+        else:
             challans = Challan.objects.select_related("violation").filter(
-                echallan_number__iexact=query
+                echallan_number=query.strip()
             )
 
     return render(request, "challan/search.html", {
