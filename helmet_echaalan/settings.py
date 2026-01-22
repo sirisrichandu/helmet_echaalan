2d85784a00388451fef313b1a860d97b334aeff6
Susmithakanukolanu20 <kanukolanususmitha20@gmail.com>
2026-01-22 20:38:49 +0530

    "Temporarily enable DEBUG to identify API error"

--- helmet_echaalan/settings.py
+++ helmet_echaalan/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-dev-key")
 
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
